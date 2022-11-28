65bb9245b81acb50a798011111b5f1b1a2ed694c
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-11-29 00:22:35 +0530

    added devise auth and login , signup functionality

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # all devise routes except for registrations
+  devise_for :users, skip: [:registrations]
+  # custom registration route and controller
+  devise_scope :user do
+    get 'users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post '/users', to: 'users/registrations#create', as: :user_registration
+  end
+
+  resources :groups, only: [:index, :new, :create, :destroy]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # for signed in users root path is groups#index
+  authenticated :user do
+    root "groups#index", as: :authenticated_root
+  end
+
+  root "welcome#index"
+end
