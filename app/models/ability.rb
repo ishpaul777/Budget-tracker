d1b831884b513f21b3587acb6126b89858f70ae4
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-11-30 18:36:33 +0530

    added authorizations

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # return if user is not logged in
+    return unless user.present?
+
+    # can access only their own profile
+    can :read, User, id: user.id
+    # can manage only their own groups and expenses
+    can :manage, Group, user_id: user.id
+    can :manage, Expense, user_id: user.id
+  end
+end
